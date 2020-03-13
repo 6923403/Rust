6136848e96b9183dd19c73b198593378e712f6c1
Then <5435@163.com>
2020-03-13 18:47:00 +0800

    Guess

---
+++ guessing_game/target/debug/deps/guessing_game-2e833feccabb8a5c.d
@@ -0,0 +1,5 @@
+/root/git/Rust/guessing_game/target/debug/deps/guessing_game-2e833feccabb8a5c: src/main.rs
+
+/root/git/Rust/guessing_game/target/debug/deps/guessing_game-2e833feccabb8a5c.d: src/main.rs
+
+src/main.rs:
---
+++ guessing_game/target/debug/deps/rand_core-1300ae331e1ce955.d
@@ -0,0 +1,7 @@
+/root/git/Rust/guessing_game/target/debug/deps/rand_core-1300ae331e1ce955.rmeta: /root/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_core-0.3.1/src/lib.rs
+
+/root/git/Rust/guessing_game/target/debug/deps/librand_core-1300ae331e1ce955.rlib: /root/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_core-0.3.1/src/lib.rs
+
+/root/git/Rust/guessing_game/target/debug/deps/rand_core-1300ae331e1ce955.d: /root/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_core-0.3.1/src/lib.rs
+
+/root/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_core-0.3.1/src/lib.rs:
