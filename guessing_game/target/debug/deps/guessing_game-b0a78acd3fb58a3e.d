da0aa0ccfbaea902bc9bdc1981db65508c574ffe
Then <5435@63.com>
2020-03-30 17:44:05 +0800

    guessing_game/

---
+++ guessing_game/target/debug/deps/guessing_game-b0a78acd3fb58a3e.d
@@ -0,0 +1,5 @@
+/root/git/Rust/guessing_game/target/debug/deps/guessing_game-b0a78acd3fb58a3e: src/main.rs
+
+/root/git/Rust/guessing_game/target/debug/deps/guessing_game-b0a78acd3fb58a3e.d: src/main.rs
+
+src/main.rs:
---
+++ guessing_game/target/debug/guessing_game.d
@@ -0,0 +1,1 @@
+/root/git/Rust/guessing_game/target/debug/guessing_game: /root/git/Rust/guessing_game/src/main.rs
