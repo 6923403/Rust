743365ca91c693b94ef077965c47b7d251365b49
Then <5435@63.com>
2020-04-10 11:38:15 +0800

    test

--- defun/target/debug/defun.d
+++
@@ -1,1 +0,0 @@
-/root/git/Rust/defun/target/debug/defun: /root/git/Rust/defun/src/main.rs
--- defun/target/debug/deps/defun-d0559302a966bc36.d
+++
@@ -1,5 +0,0 @@
-/root/git/Rust/defun/target/debug/deps/defun-d0559302a966bc36: src/main.rs
-
-/root/git/Rust/defun/target/debug/deps/defun-d0559302a966bc36.d: src/main.rs
-
-src/main.rs:
