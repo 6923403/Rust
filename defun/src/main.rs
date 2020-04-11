8f4496934e2281add0ae2ea6a1bc186ad4da4571
Then <5435@63.com>
2020-04-11 11:45:29 +0800

    define loop

--- defun/src/main.rs
+++ defun/src/main.rs
@@ -6,8 +6,106 @@ fn main() {
     another_function();
     */
 
-    test2();
+    test8();
 
+}
+
+fn test8()
+{
+    let a = [10, 20, 30, 40, 50];
+
+    for num in a.iter()
+    {
+        println!("The value is: {}", num);
+    }
+
+    test9();
+}
+
+fn test9()
+{
+    for number in (1..4).rev()
+    {
+        println!("{}!", number);
+
+    }
+
+    println!("EOF");
+}
+
+fn test7()
+{
+    let a = [10, 20, 30, 40, 50];
+
+    let mut index = 0;
+
+    while index < 5
+    {
+        println!("The value is: {}", a[index]);
+
+        index = index + 1;
+    }
+}
+
+fn test6()
+{
+    let mut number = 3;
+    while number != 0
+    {
+        print!("{}!", number);
+
+        number = number - 1;
+
+    }
+
+    println!("");
+
+    println!("LIFTOFF");
+}
+
+fn test5()
+{
+    let mut counter = 0;
+
+    let result = loop
+    {
+        counter += 1;
+
+        if counter == 10{
+            break counter * 2;
+        }
+    };
+
+    println!("The result is {}", result);
+}
+
+fn test4()
+{
+    let condition = true;
+    let number = if condition{
+        5
+    }
+    else
+    {
+        6
+    };
+
+    println!("The value of number is: {}", number);
+
+}
+
+fn test3()
+{
+    let number = 3;
+
+    if number % 4 == 0
+    {
+        println!("true");
+    }
+    else
+    {
+        println!("false");
+    }
 }
 
 /*
