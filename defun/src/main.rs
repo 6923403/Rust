b5aab29285ac5f5e89060ed4bc664d9952638fd9
Then <5435@63.com>
2020-04-11 08:55:50 +0800

    defun

--- defun/src/main.rs
+++ defun/src/main.rs
@@ -1,3 +1,69 @@
 fn main() {
-    println!("Hello, world!");
+
+    /*
+    println!("123");
+
+    another_function();
+    */
+
+    test2();
+
+}
+
+/*
+fn another_function()
+{
+    println!("456");
+}*/
+
+fn five() -> i32
+{
+    5
+}
+
+fn test1()
+{
+    /*
+    fun1(5);
+
+    fun2(7, 8);
+    */
+    fun3();
+
+    let x = five();
+
+    println!("x = {}", x);
+}
+
+fn fun1(x: i32)
+{
+    println!("The value of x = {}", x);
+}
+
+fn fun2(x: i32, y: i32)
+{
+    println!("The value of x = {}", x);
+    println!("The value of y = {}", y);
+}
+
+fn fun3()
+{
+    let x = 5;
+    let y = {
+        let x = 3;
+        x + 1
+    };
+    println!("x = {}, y = {}", x, y);
+}
+
+fn test2()
+{
+    let x = plus_one(5);
+    println!("The value of x is: {}", x);
+
+}
+
+fn plus_one(x: i32)-> i32
+{
+    x + 1
 }
