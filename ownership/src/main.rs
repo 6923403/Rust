2955583377fef5d07286261a11bab614e670bfc0
Then <5435@163.com>
2020-04-13 17:45:11 +0800

    own copy

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+fn main()
+{
+    test1()
+}
+
+fn test1()
+{
+    let s1 = String::from("hello");
+    let s2 = s1.clone();
+
+    println!("s1 = {}, s2 = {}", s1 , s2);
+
+}
+
+fn test()
+{
+
+    let mut s = String::from("hello");
+
+    s.push_str(",wordld!");
+
+    println!(" {}", s);
 }
