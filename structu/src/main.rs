627be4b029d33f55499016b98f9586bdb8c53690
Then <5435@163.com>
2020-04-25 16:44:56 +0800

    struct

--- structu/src/main.rs
+++ structu/src/main.rs
@@ -9,6 +9,17 @@ struct User
 
 fn main()
 {
+    struct Color(i32, i32, i32);
+    struct Point(i32, i32, i32);
+
+    let black = Color(0, 0, 0);
+    let origin = Color(0, 0, 0);
+}
+
+
+//fn main
+fn tes()
+{
     let user1 = User
     {
         email: String::from("5435@163.com"),
