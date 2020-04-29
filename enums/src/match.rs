a6bd81da4942ac6bb0366fc00afef07583762e14
Then <5435@163.com>
2020-04-29 20:13:01 +0800

    some

--- enums/src/match.rs
+++ enums/src/match.rs
@@ -51,9 +51,25 @@ fn value_in_cent2(Coins: Coins) -> u8
     }
 }
 
+fn plus_one(x: Option<i32>) -> Option<i32>
+{
+    match x
+    {
+        None => None,
+        Some(i) => Some(i + 1),
+    }
+}
+
 fn main()
 {
-    te1();
+    te2();
+}
+
+fn te2()
+{
+    let five = Some(5);
+    let six = plus_one(five);
+    let none = plus_one(None);
 }
 
 fn te1()
