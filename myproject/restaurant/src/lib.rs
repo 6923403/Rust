06567ff8abe177e5edf91df5009c8ac505bc5589
Then <5435@163.com>
2020-05-14 14:03:15 +0800

    super

--- myproject/restaurant/src/lib.rs
+++ myproject/restaurant/src/lib.rs
@@ -1,4 +1,5 @@
-mod front_of_house
+/*
+ * mod front_of_house
 {
     mod hosting
     {
@@ -15,4 +16,66 @@ mod front_of_house
 
         fn take_payment() {}
     }
+}
+*/
+
+mod front_of_house
+{
+    //mod hosting
+    pub mod hosting
+    {
+        pub fn add_to_waitlist()
+        {}
+    }
+}
+
+pub fn eat_at_restaurant()
+{
+    //absolute
+    crate::front_of_house::hosting::add_to_waitlist();
+
+    //relative
+    front_of_house::hosting::add_to_waitlist();
+}
+
+fn serve_order()
+{}
+
+mod back_of_house
+{
+    pub struct Breakfast
+    {
+        pub toast: String,
+        seasonal_fruit: String,
+    }
+
+    impl Breakfast
+    {
+        pub fn summer(toast: &str) -> Breakfast
+        {
+            Breakfast
+            {
+                totast: String::from(toast),
+                seasonal_fruit: String::from("peaches"),
+            }
+        }
+    }
+
+    fn fix_incorrect_order()
+    {
+        cook_order();
+        super::serve_order();
+    }
+
+    fn cook_order()
+    {}
+}
+
+pub fn eat_at_restaurants()
+{
+    let mut meal = back_of_house::Breakfast::summer("Rye");
+    meal.toast = String::from("Wheat");
+    println!("I'd like {} toast please", meal.toast);
+
+    //meal.seasonal_fruit = String::from("blueberries");
 }
