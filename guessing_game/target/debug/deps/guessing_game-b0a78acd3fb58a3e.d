37c0b236921b3b4ef4e421d93b382488ec85f8a6
Then <vcvckw@gmail.com>
2020-07-01 14:45:54 +0800

    1

--- guessing_game/target/debug/deps/guessing_game-b0a78acd3fb58a3e.d
+++
@@ -1,5 +0,0 @@
-/root/git/Rust/guessing_game/target/debug/deps/guessing_game-b0a78acd3fb58a3e: src/main.rs
-
-/root/git/Rust/guessing_game/target/debug/deps/guessing_game-b0a78acd3fb58a3e.d: src/main.rs
-
-src/main.rs:
