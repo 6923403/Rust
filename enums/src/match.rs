8351bfcccace28d862c4b14f377d3d6fd3df7ef9
Then <vcvckw@gmail.com>
2020-07-12 21:31:08 +0800

    match

--- enums/src/match.rs
+++ enums/src/match.rs
@@ -1,89 +1,53 @@
-enum Coin
-{
+enum Coin {
     Penny,
-    Nickel,
+    Nickelm,
     Dime,
     Quarter,
 }
 
-fn value_in_cents(coin: Coin) -> u8
-{
-    match coin
-    {
-        Coin::Penny => 
-        {
-            println!("LQ");
-            1
-        },
+fn value_in_cents(coin: Coin) -> u8 {
+    match coin {
+        Coin::Penny => 1,
         Coin::Nickel => 5,
         Coin::Dime => 10,
         Coin::Quarter => 25,
     }
 }
 
+fn value_in_ents2(coin: Coin) -> u8{
+    match coin {
+        Coin:;Penny => {
+            println!("Lucky penny");
+            1
+        },
+        Coin:: Nickel => 5,
+        Coin:: Dime => 10,
+        Coin::Quarter => 25,
+    }
+}
+
 #[derive(Debug)]
-enum UsState
-{
+enum UsState {
     Alabama,
     Alaska,
 }
 
-enum Coins
-{
+enum Coin {
     Penny,
     Nickel,
     Dime,
     Quarter(UsState),
 }
 
-fn value_in_cent2(Coins: Coins) -> u8
-{
-    match coin
-    {
-        Coins::Penny => 1,
-        Coins::Nickel => 5,
-        Coins::Dime => 10,
-        Coins::Quarter(state) =>
-        {
-            println!("State quarter from {:?}!", state);
-            25
-        },
-    }
-}
-
-fn plus_one(x: Option<i32>) -> Option<i32>
-{
-    match x
-    {
-        None => None,
-        Some(i) => Some(i + 1),
-    }
-}
-
 let some_u8_value = 0u8;
-match some_u8_value{
+match some_u8_value {
     1 => println!("one"),
     3 => println!("three"),
     5 => println!("five"),
     7 => println!("seven"),
+    //_ match all value
     _ => (),
 }
-
-fn main()
-{
-    te3();
-}
-
-fn te2()
-{
-    let five = Some(5);
-    let six = plus_one(five);
-    let none = plus_one(None);
-}
-
-fn te1()
-{
-  let q = value_in_cents(Coin::Penny);
-
-  let q2 = value_in_cent2(Coins::Quarter(UsState::Alaska));
+fn main() {
+    te1();
 }
