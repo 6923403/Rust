e4d3338cccbe570fd6093c356c9394ba59328ebe
vcvc <vcvckw@gmail.com>
2020-08-12 23:23:45 +0800

    vec

---
+++ Vector/src/main.rs
@@ -0,0 +1,41 @@
+/*
+ * create new vector
+ */
+let v: Vec<i32> = Vec::new();
+
+/*
+ * initial vec
+ */
+let v = vec![1, 2, 3];
+
+/*
+ * push vec
+ */
+let mut v = Vec::new();
+
+v.push(5);
+v.push(6);
+v.push(7);
+v.push(8);
+
+/*
+ * drop vec
+ */
+{
+    let v = vec![1, 2, 3, 4];
+} // drop v
+
+
+/*
+ * read vec
+ */
+let v = vec![1, 2, 3, 4, 5];
+
+let third: &i32 = &v[2];
+println!("The third element is {}", third);
+
+match v.get(2) {
+    Some(third) => println!("The third element is {}", third),
+    None => println!("There is no third element."),
+}
+
